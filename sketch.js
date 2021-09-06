dc8e1ff8852f3701946b74d79c4d9fec324ebc98
Saisha2209 <76441200+Saisha2209@users.noreply.github.com>
2021-09-06 17:46:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(1530,2000);
+ Answers=new options()
+}
+
+function draw() {
+  background("black");  
+  code();
+  Answers.display();
+  questionFive()
+  questionFour()
+  questionOne()
+  questionSix()
+  questionThree()
+  questionTwo()
+  drawSprites();
+}
